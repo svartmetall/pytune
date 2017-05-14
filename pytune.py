ce5f6d1e5a77acd01aad5f09bd2b7b1949b1edb4
svartmetall <grongad@gmail.com>
2017-05-14 19:55:31 +0300

    Frequency measurement

--- pytune.py
+++ pytune.py
@@ -1,17 +1,38 @@
-import pyaudio
 import numpy
+import pyaudio
 
+NOTE_MIN = 38   #D2
+NOTE_MAX = 62   #D4
+FSAMP = 22050
 FRAME_SIZE = 2048
-RATE = 44100
+FRAMES_PER_FFT = 16
+
+SAMPLES_PER_FFT = FRAME_SIZE * FRAMES_PER_FFT
+FREQ_STEP = FSAMP / SAMPLES_PER_FFT
+
+
+def note_to_fftbin(n):
+    return 440 * 2.0 ** ((n - 69) / 12.0) / FREQ_STEP
+
+imin = max(0, int(numpy.floor(note_to_fftbin(NOTE_MIN-1))))
+imax = min(SAMPLES_PER_FFT, int(numpy.ceil(note_to_fftbin(NOTE_MAX+1))))
+
+buf = numpy.zeros(SAMPLES_PER_FFT, dtype=numpy.float32)
+window = 0.5 * (1 - numpy.cos(numpy.linspace(0, 2*numpy.pi, SAMPLES_PER_FFT, False)))
 
 stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
                                 channels=1,
-                                rate=RATE,
+                                rate=FSAMP,
                                 input=True,
                                 frames_per_buffer=FRAME_SIZE)
 
 while stream:
-    data = numpy.fromstring(stream.read(FRAME_SIZE), dtype=numpy.int16)
-    peak = numpy.average(numpy.abs(data)) * 2
-    bars = ">"*int(50*peak/2**11)
-    print(peak, bars)
+
+    buf[:-FRAME_SIZE] = buf[FRAME_SIZE:]
+    buf[-FRAME_SIZE:] = numpy.fromstring(stream.read(FRAME_SIZE), numpy.int16)
+
+    fft = numpy.fft.rfft(buf * window)
+
+    freq = (numpy.abs(fft[imin:imax]).argmax() + imin) * FREQ_STEP
+
+    print('Frequency:', freq, 'Hz')
